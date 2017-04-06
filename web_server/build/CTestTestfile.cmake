0f9632ea07c99dcefe10e710826e4b943dcc536d
Julian Callin <j.callin@hotmail.com>
2017-04-06 04:48:31 +0000

    Untracking build/ folder

--- web_server/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/julian/Documents/senior_design_2017_backend/web_server
-# Build directory: /home/julian/Documents/senior_design_2017_backend/web_server/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(greedy_projection)
-subdirs(tests)
--- web_server/build/tests/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/julian/Documents/senior_design_2017_backend/web_server/tests
-# Build directory: /home/julian/Documents/senior_design_2017_backend/web_server/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(io_test "io_test")
-add_test(parse_test "parse_test")
-add_test(crypto_test "crypto_test")
